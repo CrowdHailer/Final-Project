3e3b787e0c72e52874a97ac0eccea8051162875c
Roy Gardiner <roy.gardiner@btopenworld.com>
2014-03-19 16:18:09 +0000

    More tests, page layout simplified

--- features/profile_user.feature
+++ features/profile_user.feature
@@ -1,6 +1,10 @@
 Feature: Show one person's details
 
+  Background:
+    Given there is a showcased user
+	
   Scenario: Show a user
+
   	When I visit the user profile page
 	  Then I should see "Profile"
 
--- features/step_definitions/profile_user.rb
+++ features/step_definitions/profile_user.rb
@@ -1,3 +1,7 @@
+Given(/^there is a showcased user$/) do
+  @showcase_user = User.create!(name: 'Pussy Galore', email: 'test@test.com', github_username: 'Geoff', uid: '2', provider: 'google')
+end
+
 When(/^I visit the user profile page$/) do
 	visit '/user/fred'
   expect(page).to have_content('Profile')
