d83f7b18ea547aa7c10c4bbb11f3f82e03f42dae
Roy Gardiner <roy.gardiner@btopenworld.com>
2014-03-19 18:49:13 +0000

    fix sign in/out bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @client_id = ENV['GITHUB_CLIENT_ID']
     @user = current_user
     # @showcase_user = User.random
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@
 #  id               :integer          not null, primary key
 #  provider         :string(255)
 #  uid              :string(255)
+
 #  name             :string(255)
 #  oauth_token      :string(255)
 #  oauth_expires_at :datetime
