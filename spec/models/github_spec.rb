bda0dbb2060d252fcc517103a6f1acbaf469e4f4
Peter <peterhsaxton@gmail.com>
2014-03-19 21:20:35 +0000

    Added response stubs github spec

--- spec/models/github_spec.rb
+++ spec/models/github_spec.rb
@@ -4,6 +4,15 @@ require 'spec_helper'
 describe Github do
   testname = 'CrowdHailer'
   let(:user) { Github.new testname}
+  before do
+    stub_request(:any, 'https://api.github.com/users/CrowdHailer').to_return(File.new('trial.txt'))
+  end
+  before do
+    stub_request(:any, 'https://api.github.com/users/CrowdHailer/repos').to_return(File.new('trial2.txt'))
+  end
+  before do
+    stub_request(:any, 'https://api.github.com/repos/CrowdHailer/CV/contents/README.md').to_return(File.new('trial3.txt'))
+  end
 
   #Blocked out to reduce calls to github api
   
