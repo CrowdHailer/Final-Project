2497705c23e31ed3b6414a8cb9b15c2a61efb16d
Colin Frankish <colin.frankish@gmail.com>
2014-03-20 11:15:06 +0000

    db update

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,11 @@ ActiveRecord::Schema.define(version: 20140320062346) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "githubs", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", force: true do |t|
     t.string   "provider"
     t.string   "uid"
