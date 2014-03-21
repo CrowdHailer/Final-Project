f00b57ef8da048adf20f210e32a617d028d831d5
Roy Gardiner <roy.gardiner@btopenworld.com>
2014-03-21 12:35:51 +0000

    Edit save working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :profile_image, :cohort)
+    params.require(:user).permit(:name, :email, :profile_image, :cohort, :bio)
   end
 end
