b45d6ba39fcd26dbf8f51e24771c0861f048f98d
Roy Gardiner <roy.gardiner@btopenworld.com>
2014-03-25 17:10:54 +0000

    fix email encoder cockup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@
 //= require turbolinks
 //= require_tree .
 
+
 $(function(){ $(document).foundation(); });
 
 function change(){
@@ -28,6 +29,7 @@ function change(){
       $('.unavailable').slideDown();
     }
 };
+
 
 
 
