45bab69e6ce327dd5ceb71589e36c5c4d3d26ae7
Peter Saxton <peterhsaxton@gmail.com>
2014-03-28 11:30:49 +0000

    fixed match method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @user = current_user
     if @showcase_user
       gon.github_username = @showcase_user.github_username
-      gon.stackoverflow_id = @showcase_user.stackoverflow.match(/\d+/)[0]
+      gon.stackoverflow_id = @showcase_user.stackoverflow.match(/\d+/)[0] if @showcase_user.stackoverflow
     end 
     redirect_to root_path, alert: 'User profile not found' unless @showcase_user
   end
